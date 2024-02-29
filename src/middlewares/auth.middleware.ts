b240fb849850b650326095f5063d211db5951c3e
gauravgit config --global user.emailgit config --global user.name gaurav <gaurav.kashyap@sachtechsolution.com>
2024-02-29 14:22:52 +0530

    boiler

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { verifyToken } from '../utils/auth.util'
 import { showOutput } from '../utils/response.util';
 import { ApiResponse } from '../utils/interfaces.util';
-///
+
 export const verifyTokenUser = async (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers;
     if (authHeader) {
@@ -12,6 +12,11 @@ export const verifyTokenUser = async (req: Request, res: Response, next: NextFun
             req.body.user = decoded?.data?.data;
             next();
         } else {
+
+            if (decoded?.data?.data?.user_type == 'admin') {
+                return showOutput(res, { status: false, message: "Unauthorized Access By Admin", data: null, other: null, code: 401 }, 401)
+            }
+
             return showOutput(res, decoded, decoded?.code)
         }
     } else {
@@ -27,10 +32,14 @@ export const verifyTokenAdmin = async (req: Request, res: Response, next: NextFu
     if (authHeader) {
         const decoded: ApiResponse = await (await verifyToken(req, res))
         // @ts-ignore
-        if (decoded.status && decoded?.data?.data?.user_type == 'admin') {
+        if (decoded?.data?.status && decoded?.data?.data?.user_type == 'admin') {
             req.body.user = decoded.data?.data;
             next();
         } else {
+            if (decoded?.data?.data?.user_type == 'user') {
+                return showOutput(res, { status: false, message: "Unauthorized Access By User", data: null, other: null, code: 401 }, 401)
+            }
+
             return showOutput(res, decoded, decoded?.code)
 
         }
