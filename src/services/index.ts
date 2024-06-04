8b28e4adb20578f86ff3e2544e336915ef1f3717
gauravgit config --global user.emailgit config --global user.name gaurav <gaurav.kashyap@sachtechsolution.com>
2024-06-04 12:26:15 +0530

    sendgrid

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,11 +1,13 @@
 
 import * as emailService from './nodemailer.service'
+import * as sendgridService from './sendGrid.service'
 import * as awsService from './aws.service'
 import * as notificationService from './notification.service'
 import * as smsService from './sms.service'
 
 export default {
     emailService,
+    sendgridService,
     awsService,
     notificationService,
     smsService,
---
+++ src/services/sendGrid.service.ts
@@ -0,0 +1,42 @@
+import nodemailer from 'nodemailer'
+import { showResponse } from "../utils/response.util";
+import { ApiResponse } from "../utils/interfaces.util";
+import { EMAIL_CREDENTIAL } from "../constants/app.constant";
+import sgMail from '@sendgrid/mail'
+import statusCodes from '../constants/statusCodes';
+
+const sendgridMail = async (to: string, subject: string, body: any, attachments: any = []): Promise<ApiResponse> => {
+    const EMAIL_HOST = await EMAIL_CREDENTIAL.EMAIL_HOST
+    const SENDGRID_API_KEY = await EMAIL_CREDENTIAL.SENDGRID_API_KEY
+    const SENDGRID_UNAME = await EMAIL_CREDENTIAL.SENDGRID_API
+    console.log(EMAIL_HOST, "EMAIL_HOST");
+    console.log(SENDGRID_UNAME, "SENDGRID_UNAME");
+    
+    sgMail.setApiKey(SENDGRID_API_KEY);
+
+    return new Promise((resolve, reject) => {
+
+        try {
+
+            const mailOptions = {
+                to:to,
+                from: SENDGRID_UNAME,
+                subject,
+                html: body,
+                // attachments
+            }
+            
+            sgMail.send(mailOptions).then((response) => {
+
+                return resolve(showResponse(true, 'Email Sent Successfully', null, statusCodes.SUCCESS));
+            }).catch((error: any) => {
+                console.log(error)
+                return reject(showResponse(false, 'Email Sent Error', error, statusCodes.API_ERROR));
+            });
+        } catch (err) {
+            return reject(showResponse(false, "Error while sending Email", err, statusCodes.API_ERROR));
+        }
+    });
+}
+
+export { sendgridMail } 
