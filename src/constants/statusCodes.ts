fb01c235693205a6e216a812c145d7ffbb3913ec
gauravgit config --global user.emailgit config --global user.name gaurav <gaurav.kashyap@sachtechsolution.com>
2024-07-24 17:31:50 +0530

    account disable http code

--- src/constants/statusCodes.ts
+++ src/constants/statusCodes.ts
@@ -5,7 +5,7 @@ const statusCodes = {
     REFRESH_TOKEN_ERROR: 403,         // Refresh Token Expiry error
     FILE_UPLOAD_ERROR: 422,        // unprocessible entity
     VALIDATION_ERROR: 417,         // expectation failed
-    ACCOUNT_DISABLED: 403,        //  no longer available
+    ACCOUNT_DISABLED: 409,        //  no longer available
     ACCOUNT_DELETED: 410,        //  no longer available
     SERVER_TRYCATCH_ERROR: 500, // server error 
     TOO_MANY_REQUESTS: 429, // Too Many Request
