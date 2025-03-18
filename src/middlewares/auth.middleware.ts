363ffdd7e73955909b93e5d9197d298d28ab14e6
gauravgit config --global user.emailgit config --global user.name gaurav <gaurav.kashyap@sachtechsolution.com>
2025-03-18 16:58:18 +0530

    mddleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -12,11 +12,10 @@ export const verifyTokenUser = async (req: Request, res: Response, next: NextFun
         if (decoded.status && decoded?.data?.user_type == 'user') {
             req.body.user = decoded.data;
             next();
-        } else if (decoded.status && decoded?.data?.user_type == 'admin') {
-            return showOutput(res, { status: false, message: responseMessages.users.invalid_user, data: null, code: statusCodes.AUTH_TOKEN_ERROR }, statusCodes.AUTH_TOKEN_ERROR)
-
-        } else { //false case
-            return showOutput(res, decoded, decoded?.code) //return error message if token expire account deactiavted etc... 
+        } else if (!decoded.status) {
+            return showOutput(res, { status: decoded.status, message: decoded?.message, data: null, code: decoded?.code }, decoded?.code)
+        } else {
+            return showOutput(res, { status: false, message: "Invalid User", data: null, code: statusCodes.AUTH_TOKEN_ERROR }, statusCodes.AUTH_TOKEN_ERROR)
         }
     } catch (error) {
         return showOutput(res, { status: false, message: responseMessages.users.unauthorised_user, data: error, code: statusCodes.AUTH_TOKEN_ERROR }, statusCodes.AUTH_TOKEN_ERROR)
@@ -31,12 +30,8 @@ export const verifyTokenAdmin = async (req: Request, res: Response, next: NextFu
         if (decoded.status && decoded?.data?.user_type == 'admin') {
             req.body.user = decoded.data;
             next();
-        } else if (decoded.status && decoded?.data?.user_type == 'user') {
-
-            return showOutput(res, { status: false, message: responseMessages.admin.invalid_admin, data: null, code: statusCodes.AUTH_TOKEN_ERROR }, statusCodes.AUTH_TOKEN_ERROR)
-
         } else {
-            return showOutput(res, decoded, decoded?.code) //return error message if token expire account deactiavted etc... 
+            return showOutput(res, { status: false, message: "Invalid Admin", data: null, code: statusCodes.AUTH_TOKEN_ERROR }, statusCodes.AUTH_TOKEN_ERROR)
         }
     } catch (error) {
         return showOutput(res, { status: false, message: responseMessages.admin.unauthorized_access, data: error, code: statusCodes.AUTH_TOKEN_ERROR }, statusCodes.AUTH_TOKEN_ERROR)
@@ -50,8 +45,10 @@ export const verifyTokenBoth = async (req: Request, res: Response, next: NextFun
         if (decoded.status && decoded?.data?.user_type == 'user' || decoded?.data?.user_type == 'admin') {
             req.body.user = decoded.data;
             next();
+        } else if (!decoded.status) {
+            return showOutput(res, { status: decoded.status, message: decoded?.message, data: null, code: decoded?.code }, decoded?.code)
         } else {
-            return showOutput(res, decoded, decoded?.code) //return error message if token expire account deactiavted etc...
+            return showOutput(res, { status: false, message: "Invalid User", data: null, code: statusCodes.AUTH_TOKEN_ERROR }, statusCodes.AUTH_TOKEN_ERROR)
         }
 
     } catch (error) {
